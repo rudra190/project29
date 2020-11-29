f76ceafb60f6060b6ae690471508624c7d8448dc
rudra190 <72394908+rudra190@users.noreply.github.com>
2020-11-29 16:30:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var a;
+var circles=[];
+function setup() {
+
+  stroke(255)
+  
+ // camera=new Camera(width/2,height/2,0.5);
+  //camera.on();
+  a=height;
+  circles.push(width/2)
+}
+
+function setup(){
+  var canvas = createCanvas(1200,400);
+ 
+
+  ground = new Ground(600,height,1200,20);
+}
+
+
+function draw() {
+  //camera.zoom=camera.zoom+1
+  background(0);  
+  
+  
+  a=a-1;
+  //camera.zoom=camera.zoom+0.01
+ //camera.position={x:width/2,y:a}
+ 
+  
+  for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+ // camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+ drawSprites();
+}
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+    if(keyIsDown(RIGHT_ARROW))
+    {
+      camera.position.x=camera.position.x+10;
+    }
+  }
+} 
